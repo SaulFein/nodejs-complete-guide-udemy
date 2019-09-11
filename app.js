e66847322aa0f38ec99bd1e4772cec6272a3c126
SaulFein <brandon.feinstein@hotmail.com>
2019-09-10 21:48:53 -0700

    more stuff

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware !")
-    next(); //Allows the request to continue to next middleware in line
-});
-
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log("In the middleware ! 2")
-    res.send("<h1>Test</h1>");
+    res.send("<h1>The Add Product Page</h1>");
 });
 
-const server = http.createServer(app);
+// this order is important
+app.use('/', (req, res, next) => {
+    console.log("This Always Runs");
+    res.send("<h1> Middle ware /")
+});
 
-server.listen(3000);
+app.listen(3000);
