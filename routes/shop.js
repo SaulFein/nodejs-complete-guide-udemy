aa7e6d434aa97db0d0f972f0d52dc18e55960961
SaulFein <brandon.feinstein@hotmail.com>
2019-09-14 08:57:41 -0700

    latest

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
+const rootDir = require('../util/path');
 
 router.get('/', (req, res, next) => {
-    res.send("<h1> Middle ware /")
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 });
 
 
