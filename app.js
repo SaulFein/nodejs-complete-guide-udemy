8762d17976a49140e38e56e6df38162225bc9ea8
SaulFein <brandon.feinstein@hotmail.com>
2019-09-14 15:20:24 -0700

    pug templating engine

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
+app.set('view engine', 'pug' );
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -10,11 +13,11 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 })
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,13 @@ const express = require('express');
 
 const router = express.Router();
 
+const adminData = require('./admin');
+
 const rootDir = require('../util/path');
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const products =  adminData.products;
+    res.render('shop', {pageTitle: 'Shop', prods: products, path: '/'});
 });
 
 
